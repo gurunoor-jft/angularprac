35baaa77208c8356a365c3aa9eac4ff8958ce456
gurunoor-jft <gurunoor.singh@jellyfishtechnologies.com>
2021-03-04 12:59:47 +0530

    Done Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
 import { ChangeTextDirective } from './change-text.directive';
-import {SqrtPipe} from './app.sqrt'
+import {SqrtPipe} from './app.sqrt';
 
 @NgModule({
   declarations: [
     SqrtPipe,
     AppComponent,
     NewCmpComponent,
-    ChangeTextDirective
+    ChangeTextDirective,
+    RoutingComponent
   ],
   imports: [
     BrowserModule,
